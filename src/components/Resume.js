568dc40052a5adfd0893eccfe2f25af66106a328
Onaje Oladuwa <onaje.oladuwa@gmail.com>
2021-10-06 09:25:30 -0400

    working

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,19 +1,19 @@
-import React from "react";
-import PdfViewer from "./PdfViewer";
-// import "./resume.css";
+// import React from "react";
+// import PdfViewer from "./PdfViewer";
+// // import "./resume.css";
 
-const Resume = () => {
-  return (
-    <section id="resumeContainer">
-      <div className="container  laptop:bg-irish-green bg-goldenrod-yellow flex flex-col items-center justify-center p-12 min-w-full">
-        <div className="container flex justify-center laptop:bg-goldenrod-yellow w-11/12 p-12">
-          <div className="flex justify-center laptop:bg-nd-blue p-8 w-11/12">
-            <PdfViewer />
-          </div>
-        </div>
-      </div>
-    </section>
-  );
-};
+// const Resume = () => {
+//   return (
+//     <section id="resumeContainer">
+//       <div className="container  laptop:bg-irish-green bg-goldenrod-yellow flex flex-col items-center justify-center p-12 min-w-full">
+//         <div className="container flex justify-center laptop:bg-goldenrod-yellow w-11/12 p-12">
+//           <div className="flex justify-center laptop:bg-nd-blue p-8 w-11/12">
+//             <PdfViewer />
+//           </div>
+//         </div>
+//       </div>
+//     </section>
+//   );
+// };
 
-export default Resume;
+// export default Resume;
